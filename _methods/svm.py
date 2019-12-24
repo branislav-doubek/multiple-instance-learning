cbee27b41899824ef844a4458e788284ff4e3eb6
Branislav Doubek <37004830+branislav-doubek@users.noreply.github.com>
2019-12-24 17:54:39 +0100

    Add files via upload

--- _methods/svm.py
+++ _methods/svm.py
@@ -7,15 +7,18 @@ class Mi_SVM:
     @ignore_warnings(category=ConvergenceWarning)
     def svm_train(self):
         self.logger.debug('Starting svm\n')
+        print('Start SVM')
         clf = LinearSVC(loss='squared_hinge', random_state=0, max_iter=1, C=1/self.lamb)
         epochs = 0
         labels_changed = True
         last_labels = []
         while labels_changed and epochs <= self.max_iterations:
-            loss = self.loss_function()
-            if epochs % 1 == 0:
-                self.logger.error('Epoch={}: Loss = {}'.format(epochs,loss))
-            self.loss_history.append(loss)
+            print(epochs)
+            #loss = self.loss_function()
+            #if epochs % 2 == 0:
+            #    self.logger.error('Epoch={}: Loss = {}'.format(epochs,loss))
+            #self.loss_history.append(loss)
+
             labels = []
             features = []
             for bag in self.training_bags:
