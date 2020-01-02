571abb2cf610731559c6d12acff468079f2d5b59
Branislav Doubek <37004830+branislav-doubek@users.noreply.github.com>
2020-01-02 13:32:09 +0100

    Add files via upload

--- data/camelyon.py
+++ data/camelyon.py
@@ -3,13 +3,14 @@ import numpy as np
 import os
 from _methods.utilities import shuffle_dataset, multiply_features
 import pickle
+import random
 
 def get_dataset(args):
     """
     Loads and batches Camelyon dataset into feature and bag label lists
     :return: list(features), list(bag_labels)
     """
-    filepath = os.getcwd() + '/data/{}_dataset'.format(args.dataset)
+    filepath = os.getcwd() + '/data/{}_dataset_{}'.format(args.dataset, args.rs)
     if (os.path.exists(filepath)):
         print('Dataset loaded')
         dataset =  joblib.load(filepath)
@@ -35,7 +36,8 @@ class Dataset():
         self.training_labels = []
         self.testing_labels = []
         self.rs = args.rs
-
+        self.pos_bags = []
+        self.neg_bags = []
         # Appends all pkl files to lists
         filepath = os.getcwd()
         for file in os.listdir(filepath + '/data/camelyon_features/testing/normal/'):
@@ -57,31 +59,38 @@ class Dataset():
             with open(instance, 'rb') as tfh:
                 tsd = pickle.load(tfh)
                 if np.array(len(tsd['instances'][0])) == 32:
-                    self.training_data.append(np.array(tsd['instances']))
-                    self.training_labels.append(tsd['bag_label'])
+                    self.pos_bags.append(np.array(tsd['instances']))
 
         for instance in list_of_negative_training_bags:  # negative instances
             with open(instance, 'rb') as tfh:
                 tsd = pickle.load(tfh)
                 if np.array(len(tsd['instances'][0])) == 32:
-                    self.training_data.append(np.array(tsd['instances']))
-                    self.training_labels.append(-tsd['bag_label'])
+                    self.neg_bags.append(np.array(tsd['instances']))
 
         for instance in list_of_positive_testing_bags:  # positive instances
             with open(instance, 'rb') as tfh:
                 tsd = pickle.load(tfh)
                 if np.array(len(tsd['instances'][0])) == 32:
-                    self.testing_data.append(np.array(tsd['instances']))
-                    self.testing_labels.append(tsd['bag_label'])
+                    self.pos_bags.append(np.array(tsd['instances']))
 
         for instance in list_of_negative_testing_bags:  # negative instances
             with open(instance, 'rb') as tfh:
                 tsd = pickle.load(tfh)
                 if np.array(len(tsd['instances'][0])) == 32:
-                    self.testing_data.append(np.array(tsd['instances']))
-                    self.testing_labels.append(-tsd['bag_label'])
-        self.training_data, self.training_labels = shuffle_dataset(self.training_data, self.training_labels, self.rs)
+                    self.neg_bags.append(np.array(tsd['instances']))
+        self.random_shuffle()
+
+    def random_shuffle(self):
+        random.seed(self.rs)
+        random.shuffle(self.pos_bags)
+        random.shuffle(self.neg_bags)
+        self.training_data = self.pos_bags[:int(len(self.pos_bags)/2)] + self.neg_bags[:int(len(self.neg_bags)/2)]
+        self.training_labels = [1 for _ in range(int(len(self.pos_bags)/2))] + [-1 for _ in range(int(len(self.neg_bags)/2))]
+        self.testing_data = self.pos_bags[int(len(self.pos_bags)/2):] + self.neg_bags[int(len(self.neg_bags)/2):]
+        self.testing_labels = [1 for _ in range(int(len(self.pos_bags)/2))] + [-1 for _ in range(int(len(self.neg_bags)/2))]
         self.testing_data, self.testing_labels = shuffle_dataset(self.testing_data, self.testing_labels, self.rs)
+        self.training_data, self.training_labels = shuffle_dataset(self.training_data, self.training_labels, self.rs)
+
 
     def return_testing_set(self):
         return self.testing_data, self.testing_labels
